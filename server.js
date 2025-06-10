d7e6d34ee12deb70611cccf6a58aead297c0722c
oreakinwole <oreakinwole@gmail.com>
2025-06-10 12:40:14 +0100

    refactor

---
+++ server.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import app from './src/app.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
+
+  
