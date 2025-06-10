2cacea2b45a23940c0bec4d0de844b974ae7d477
oreakinwole <oreakinwole@gmail.com>
2025-06-10 06:57:36 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
