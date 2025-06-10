3fea587a4f6c58b464308e9c67fa4b90a90eae63
oreakinwole <oreakinwole@gmail.com>
2025-06-10 16:04:21 +0100

    feat: edit error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,9 @@
 const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: err.message });
+  // console.error(err.stack);
+  res.status(500).json({
+    error: "Internal server error",
+    message: err?.message || "Something went wrong!",
+  });
 };
 
 export default errorHandler;
