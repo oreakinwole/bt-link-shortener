3838d840c6e3f69a6640928bcc61a7f59369429a
oreakinwole <oreakinwole@gmail.com>
2025-06-10 17:24:33 +0100

    feat: add 4040 hanlde

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import express from 'express';
-import urlShortenerRoutes from './routes/urlShortener.js';
-import errorHandler from './middlewares/errorHandler.js';
+import express from "express";
+import urlShortenerRoutes from "./routes/urlShortener.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 app.use(express.json());
 
-app.use('api/v1/shorten', urlShortenerRoutes);
+app.use("api/v1/shorten", urlShortenerRoutes);
 app.use(errorHandler);
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Not found",
+    message: "The requested resource was not found",
+  });
+});
 
 export default app;
